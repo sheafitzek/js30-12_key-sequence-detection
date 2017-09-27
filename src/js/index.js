d57366eda5ecb0d4dd40e85cc18d19a5aac1da46
sheafitzek <agg817@sbcglobal.net>
2017-09-26 23:41:41 -0500

    add folder structure

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+const feature = (function() { // "feature" is a placeholder for an element or section of html
+	config: {
+		container: document.querySelector(`.targetClassOrId`); // "targetClassOrId" is the class (or id) of the "feature" we are working on
+	},
+
+	init(config) {
+		
+	},
+
+	publish() {
+		
+	},
+
+	subscribe() {
+		
+	},
+
+	return {
+		init: init,
+	};
+
+})();
+
+window.onload = app.init;
