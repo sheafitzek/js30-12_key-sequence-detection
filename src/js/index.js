c2556e83758097a9d1bb3066f0ba26a1d3361529
sheafitzek <agg817@sbcglobal.net>
2017-09-28 00:21:50 -0500

    added secret code

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,33 @@
-const feature = (function() { // "feature" is a placeholder for an element or section of html
+const app = {
 	config: {
-		container: document.querySelector(`.targetClassOrId`); // "targetClassOrId" is the class (or id) of the "feature" we are working on
+		container: document.querySelector(`.targetClassOrId`), // "targetClassOrId" is the class (or id) of the "app" we are working on
+		subContainer: document.querySelector(`.targetClassOrId`), // a child of "container"
+		secretCode: [`ArrowUp`, `ArrowUp`, `ArrowDown`, `ArrowDown`, `ArrowLeft`, `ArrowRight`, `ArrowLeft`, `ArrowRight`, `b`, `a`, `Enter`],
+		pressed: [],
 	},
 
 	init(config) {
-		
+		app.bindUiActions();
 	},
 
-	publish() {
-		
+	bindUiActions() {
+		window.addEventListener(`keyup`, (e)=> {
+			app.parseKeyCode(e);
+		});
 	},
 
-	subscribe() {
-		
+	parseKeyCode(e) {
+		const pressed = app.config.pressed;
+		const code = app.config.secretCode;
+
+		app.config.pressed.push(e.key);
+		pressed.splice(-code.length - 1, pressed.length - code.length);
+		console.log(e.key);
+		console.log(pressed);
+		[...pressed].join(` `) === [...code].join(` `)
+			&& console.log(`you did it!`);
 	},
 
-	return {
-		init: init,
-	};
-
-})();
+};
 
 window.onload = app.init;
